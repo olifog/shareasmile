5fa25d389cf77a914c7cde3ac3053f8dff96d501
Oli Fog <oliver@fogelin.com>
2020-11-07 15:09:28 +0000

    google api email scanner

---
+++ emailscanner.py
@@ -0,0 +1,49 @@
+import pickle
+import os.path
+import time
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.modify']
+
+
+def get_message(service):
+    searchq = 'is:unread from:email@shareasmiletoday.co.uk'
+    results = service.users().messages().list(userId='me', q=searchq, maxResults=1).execute()
+
+    try:
+        id = results['messages'][0]['id']
+        email = service.users().messages().get(userId='me', id=id, format='full').execute()
+        service.users().messages().modify(userId='me', id=id, body={'removeLabelIds': ["UNREAD"]}).execute()
+    except IndexError:
+        return
+
+    print(email)
+
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+
+    while True:
+        time.sleep(10)
+        get_message(service)
+
+
+if __name__ == '__main__':
+    main()
