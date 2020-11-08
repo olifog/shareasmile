827a91dcd7af35948c02712380a3746596e05157
Oli Fog <oliver@fogelin.com>
2020-11-08 18:50:15 +0000

    Setting up mongodb

--- smile.py
+++ smile.py
@@ -1,10 +1,16 @@
 from fastapi import Security, Depends, FastAPI, HTTPException
 from fastapi.security.api_key import APIKeyQuery, APIKey
 from starlette.responses import RedirectResponse
+import motor.motor_asyncio
+import aiosmtplib
+import asyncio
+from datetime import datetime
+from typing import Optional
 import uvicorn
 import json
 
-API_KEY = json.load(open('key.json', 'rb'))['key']
+credentials = json.load(open('secret.json', 'rb'))
+API_KEY = credentials['key']
 api_key_query = APIKeyQuery(name='api-key', auto_error=False)
 
 app = FastAPI(docs_url=None)
@@ -17,15 +23,46 @@ async def get_api_key(api_key_query: str = Security(api_key_query)):
         raise HTTPException(status_code=403, detail="Could not validate credentials")
 
 
+@app.on_event("startup")
+async def startup_event():
+    loop = asyncio.get_running_loop()
+
+    uri = f"mongodb://api:{credentials['mongopass']}@olifog.me:27017/?authSource=test"
+    app.motor_client = motor.motor_asyncio.AsyncIOMotorClient(uri, io_loop=loop)
+    app.db = app.motor_client.smile
+
+
 @app.get("/")
 async def root():
     response = RedirectResponse(url='https://fog.codes/')
     return response
 
 
-@app.get("/secure-endpoint")
-async def secure_endpoint(api_key: APIKey = Depends(get_api_key)):
-    return {"message": "Secure Endpoint"}
+@app.get("/new-voucher")
+async def new_voucher(
+        sku: str,
+        sender: str,
+        recipient_email: str,
+        recipient_name: str,
+        message: Optional[str] = None,
+        api_key: APIKey = Depends(get_api_key)):
+    document = {
+        'sku': sku,
+        'sender': sender,
+        'recipient': {
+            'email': recipient_email,
+            'name': recipient_name
+        },
+        'orderDate': datetime.now(),
+        'message': message
+    }
+
+    resp = await app.db.vouchers.insert_one(document)
+
+    # generate QR code
+    # send email
+
+    return {"message": "Success"}
 
 
 if __name__ == "__main__":
