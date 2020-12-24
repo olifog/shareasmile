9e5757b4850792bbaa951595360eb5d936a53106
Oli Fog <oliver@fogelin.com>
2020-12-24 14:15:41 +0000

    Business login form

--- smile.py
+++ smile.py
@@ -1,33 +1,107 @@
 import os
 import pickle
-from fastapi import Security, Depends, FastAPI, HTTPException
+import asyncio
+import uvicorn
+import json
+import io
+import base64
+
+from fastapi import Security, Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, OAuth2
+from fastapi.security.base import SecurityBase
 from fastapi.security.api_key import APIKeyQuery, APIKey
-from starlette.responses import RedirectResponse, StreamingResponse
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.staticfiles import StaticFiles
+from fastapi.encoders import jsonable_encoder
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.openapi.utils import get_openapi
+
+from starlette.responses import RedirectResponse, StreamingResponse, Response, JSONResponse
+from starlette.status import HTTP_403_FORBIDDEN
+from starlette.requests import Request
+
+from jose import jwt
+from passlib.context import CryptContext
+
+from pydantic import BaseModel
+
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from PIL import Image
+from google.auth.transport.requests import Request as GoogleRequest
+
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
+
 import motor.motor_asyncio
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+from PIL import Image
 import segno
-import asyncio
-import base64
-from datetime import datetime
+
 from typing import Optional
-import uvicorn
-import json
-import io
+from datetime import datetime, timedelta
+
 
 credentials = json.load(open('secret.json', 'rb'))
 API_KEY = credentials['key']
+SECRET_KEY = credentials['secret']
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 10080
+
 api_key_query = APIKeyQuery(name='api-key', auto_error=False)
 
-app = FastAPI(docs_url=None)
+#app = FastAPI(docs_url=None)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 mask = Image.open('./qrlogo.png')
+
+
+def get_oauth2_token(request: Request):
+    authorization: str = request.cookies.get("Authorization")
+    scheme, param = get_authorization_scheme_param(authorization)
+
+    if scheme.lower() != "bearer":
+        return None
+    return param
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def authenticate_user(name: str, password: str):
+    user = await app.db.businesses.find_one({'name': name})
+    if not user:
+        return False
+    if not verify_password(password, user['pass']):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 async def get_api_key(api_key_query: str = Security(api_key_query)):
@@ -99,7 +173,7 @@ async def startup_event():
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            creds.refresh(GoogleRequest())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
@@ -110,18 +184,35 @@ async def startup_event():
     app.service = build('gmail', 'v1', credentials=creds)
 
 
-@app.get("/")
-async def root():
-    response = RedirectResponse(url='https://fog.codes/')
+@app.post("/login", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user['name']}, expires_delta=access_token_expires
+    )
+
+    token = jsonable_encoder(access_token)
+
+    response = Response()
+    response.set_cookie(
+        "Authorization",
+        value=f"Bearer {token}",
+        domain="smile.fog.codes",
+        httponly=True,
+        max_age=1800,
+        expires=1800,
+    )
     return response
 
 
-@app.get("/redeem/{voucherid}")
-async def redeem(voucherid):
-    return {'message': 'success'}
-
-
-@app.get("/new-voucher")
+@app.post("/new-voucher")
 async def new_voucher(
         sku: str,
         sender_name: str,
@@ -161,6 +252,39 @@ async def new_voucher(
     asyncio.create_task(check_sent(res['threadId']))
 
     return {"message": "Success"}
+
+
+@app.get("/")
+async def root():
+    response = RedirectResponse(url='https://fog.codes/')
+    return response
+
+
+@app.get("/redeem/{voucherid}")
+async def redeem(voucherid, request: Request):
+    token = get_oauth2_token(request)
+    try:
+        voucher = await app.db.vouchers.find_one({'_id': ObjectId(voucherid)})
+    except InvalidId:
+        voucher = None
+
+    if not voucher:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="That voucher is either invalid or has expired!",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    product = await app.db.products.find_one({'sku': voucher['sku']})
+    business = await app.db.businesses.find_one({'_id': ObjectId(product['business'])})
+
+    if not token:
+        response = RedirectResponse(url=f"/static/login.html?name={business['name']}&redeem={voucherid}")
+        return response
+
+    # send money to the business
+    await app.db.vouchers.delete_many({'_id': ObjectId(voucherid)})
+    return {'message': 'Voucher redeemed!'}
 
 
 if __name__ == "__main__":
